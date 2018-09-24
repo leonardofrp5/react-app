e12b27e19b98ca8bec4713cb16e03c62680a06c9
leonardo <leonardofrp@hotmail.com>
2018-09-23 21:43:55 -0500

    se agregan iconos de redes sociales para el footer

--- src/components/Global/Footer.js
+++ src/components/Global/Footer.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import logo_facebook from './images/facebook.png';
+import logo_linkein from './images/linkedin.png';
+import logo_git from './images/github-logo.png';
+import logo_twitter from './images/twitter.png';
+import logo_instagram from './images/instagram.png';
 import './css/Footer.css';
 
 class Footer extends Component {
   render() {
     return (
       <div className="Footer">
-       <span>Copyright -- Leonardo Romero Perez</span>
+        <a href="">
+          <img src={logo_facebook} alt="logo_facebook" />
+          <img src={logo_linkein} alt="logo_linkein" />
+          <img src={logo_git} alt="logo_git" />
+          <img src={logo_twitter} alt="logo_twitter" />
+          <img src={logo_instagram} alt="logo_instagram" />
+        </a>
+        <div className="Copiright">
+          <span>Copyright -- Leonardo Romero Perez</span>
+        </div>
       </div>
     );
   }
